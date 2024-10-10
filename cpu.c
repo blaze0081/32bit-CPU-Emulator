841ec1af68c620a6fa8e36d47631aabf13a83b39
blaze0081 <bhaskarmishra008@gmail.com>
2024-10-11 03:22:26 +0530

    first draft

---
+++ cpu.c
@@ -0,0 +1,299 @@
+#include "cpu.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#define memory_size 1024
+
+//Put the entire FILE into memory to read
+int32_t *cpuCreateMemory(FILE *program, size_t stackCapacity, int32_t **stackBottom){
+
+    int32_t *memoryPointer = malloc(memory_size*sizeof(int32_t));
+    int32_t *memoryPointerCopy;
+
+    size_t allocatedBlocks = 1;
+    uint32_t actualIndex = 0;
+    uint32_t maxIndex = 1023;
+    int32_t input = 0;
+    int32_t instruction[4] = { 0 };
+    int8_t counter = 3;
+
+    while((input = fgetc(program)) != EOF){
+        instruction[counter] = input;
+        counter--;
+        if (counter < 0){
+            if(maxIndex < actualIndex){
+                //32 hexcodes limit right now
+                // TODO: increase this size
+            }
+
+            for(int8_t i = 0; i<4; i++){
+                *(memoryPointer + actualIndex) = (*(memoryPointer + actualIndex) << 8) | (instruction[i] & 0xff);
+                /*
+                (instruction[i] & 0xff): takes only 2bytes of data, nullifies other if present
+                (*(memoryPointer + actualIndex) << 8): calculates the address of memory at which data should be stored and creates 8 bits(2 bytes) of space
+                */
+            }
+            actualIndex++;
+            counter = 3;
+        }
+    }
+
+    //Check for memory left and stack size
+
+    *stackBottom = memoryPointer + maxIndex;
+    memset(memoryPointer + actualIndex, 0, (maxIndex - actualIndex + 1) * sizeof(int32_t));
+    return memoryPointer;
+}
+
+static void resetRegisters(struct cpu *cpu){
+    cpu->A = 0;
+    cpu->B = 0;
+    cpu->C = 0;
+    cpu->D = 0;
+    cpu->status = 0;
+    cpu->stackSize = 0;
+    cpu->instructionPointer = 0;
+    cpu->result = 0;
+}
+
+void cpuCreate(struct cpu *cpu, int32_t *memory, int32_t *stackBottom, size_t stackCapacity){
+    resetRegisters(cpu);
+    cpu->memory = memory;
+    cpu->stackBottom = stackBottom;
+    cpu->stackLimit = stackBottom - stackCapacity;
+}
+
+void cpuDestroy(struct cpu *cpu){
+    free(cpu->memory);
+    resetRegisters(cpu);
+    cpu->memory = NULL;
+    cpu->stackBottom = NULL;
+    cpu->stackLimit = NULL;
+}
+
+void cpuReset(struct cpu *cpu){
+    memset(cpu->stackLimit + 1, 0, (cpu->stackBottom - cpu->stackLimit) * sizeof(int32_t));
+    resetRegisters(cpu);
+}
+
+int cpuStatus(struct cpu *cpu){
+    return cpu->status;
+}
+
+int32_t cpuPeek(struct cpu *cpu, char reg){
+    switch (reg) {
+    case 'A':
+        return cpu->A;
+    case 'B':
+        return cpu->B;
+    case 'C':
+        return cpu->C;
+    case 'D':
+        return cpu->D;
+    case 'S':
+        return cpu->stackSize;
+    case 'I':
+        return cpu->instructionPointer;
+    case 'R':
+        return cpu->result;
+    default:
+        return 0; // Invalid register
+    }
+}
+
+int cpuRun(struct cpu *cpu, size_t steps){
+    if (cpuStatus(cpu) != cpuOK) {
+        return 0;
+    }
+
+    uint32_t counter = 0;
+    while (steps > counter) {
+        cpuStep(cpu);
+        counter++;
+
+        if (cpu->status == cpuHalted) {
+            break;
+        }
+        if (cpu->status != cpuOK) {
+            return counter * (-1); // Error code
+        }
+    }
+    return counter; // OK
+}
+
+static int32_t next32Bits(struct cpu *cpu, int32_t addition){
+    if (cpu->memory + cpu->instructionPointer + addition > cpu->stackLimit) {
+        cpu->status = cpuInvalidAddress;
+        return 0;
+    }
+    return *(cpu->memory + cpu->instructionPointer + addition);
+}
+
+static int32_t getRegister(struct cpu *cpu, int32_t index){
+    switch (index) {
+    case 0:
+        return cpu->A;
+    case 1:
+        return cpu->B;
+    case 2:
+        return cpu->C;
+    case 3:
+        return cpu->D;
+    case 4:
+        return cpu->result;
+    }
+    return 0;
+}
+
+static int operationInstruction(struct cpu *cpu, char operand){
+    int32_t registerIndex = next32Bits(cpu, 1);
+    if (cpuStatus(cpu) == cpuInvalidAddress) {
+        return 0;
+    }
+
+    if (registerIndex < 0 || registerIndex > 3) {
+        cpu->status = cpuIllegalOperand;
+        return 0;
+    }
+
+    int32_t registerInValue = getRegister(cpu, registerIndex);
+    if (operand == '/' && registerInValue == 0) {
+        cpu->status = cpuDivByZero;
+        return 0;
+    }
+
+    int32_t value = cpuPeek(cpu, 'A');
+
+    switch (operand) {
+    case '+':
+        value += registerInValue;
+        break;
+    case '-':
+        value -= registerInValue;
+        break;
+    case '*':
+        value *= registerInValue;
+        break;
+    case '/':
+        value /= registerInValue;
+        break;
+    }
+    cpu->A = value;
+
+#ifdef BONUS_JMP
+    cpu->result = value;
+#endif
+
+    cpu->instructionPointer += 2;
+    return 1;
+}
+
+static void setRegister(struct cpu *cpu, int32_t index, int32_t value)
+{
+    switch (index) {
+    case 0:
+        cpu->A = value;
+        break;
+    case 1:
+        cpu->B = value;
+        break;
+    case 2:
+        cpu->C = value;
+        break;
+    case 3:
+        cpu->D = value;
+        break;
+    }
+}
+
+static int movInstruction(struct cpu *cpu)
+{
+    int32_t registerIndex = next32Bits(cpu, 1);
+    int32_t numValue = next32Bits(cpu, 2);
+    if (cpuStatus(cpu) == cpuInvalidAddress) {
+        return 0;
+    }
+
+    if (registerIndex < 0 || registerIndex > 3) {
+        cpu->status = cpuIllegalOperand;
+        return 0;
+    }
+
+    setRegister(cpu, registerIndex, numValue);
+    cpu->instructionPointer += 3;
+    return 1;
+}
+
+enum instructionKeyWords
+{
+    nope,
+    halt,
+    add,
+    sub,
+    mul,
+    divI,
+    inc,
+    dec,
+    loop,
+    mov,
+    load,
+    store,
+    in,
+    get,
+    out,
+    put,
+    swap,
+    push,
+    pop,
+    cmp,
+    jmp,
+    jz,
+    jnz,
+    jgt,
+    call,
+    ret
+};
+
+int cpuStep(struct cpu *cpu)
+{
+
+    if (cpuStatus(cpu) != cpuOK) {
+        return 0;
+    }
+
+    if (cpu->memory + cpu->instructionPointer < cpu->memory ||
+            cpu->memory + cpu->instructionPointer > cpu->stackLimit) {
+        cpu->status = cpuInvalidAddress;
+        return 0;
+    }
+
+    int32_t instruction = next32Bits(cpu, 0);
+    if (cpuStatus(cpu) == cpuInvalidAddress) {
+        return 0;
+    }
+
+    switch (instruction) {
+    case nope:
+        cpu->instructionPointer++;
+        return 1;
+    case halt:
+        cpu->status = cpuHalted;
+        cpu->instructionPointer++;
+        return 0;
+    case add:
+        return operationInstruction(cpu, '+');
+    case sub:
+        return operationInstruction(cpu, '-');
+    case mul:
+        return operationInstruction(cpu, '*');
+    case divI:
+        return operationInstruction(cpu, '/');
+    case mov:
+        return movInstruction(cpu);
+    default:
+        cpu->status = cpuIllegalInstruction;
+        return 0;
+    }
+}
--- main.c
+++ main.c
@@ -1,5 +1,66 @@
+#include "cpu.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <limits.h>
 
-int main(){
-    
+const char *statusName(enum cpuStatus status){
+    switch (status) {
+        case cpuOK:
+            return "cpuOK";
+        case cpuHalted:
+            return "cpuHalted";
+        case cpuIllegalInstruction:
+            return "cpuIllegalInstruction";
+        case cpuIllegalOperand:
+            return "cpuIllegalOperand";
+        case cpuInvalidAddress:
+            return "cpuInvalidAddress";
+        case cpuInvalidStackOperation:
+            return "cpuInvalidStackOperation";
+        case cpuDivByZero:
+            return "cpuDivByZero";
+        case cpuIOError:
+            return "cpuIOError";
+        default:
+            fprintf(stderr, "BUG: Unknown status (%d)\n", status);
+            abort();
+    }
+    printf("\n");
+}
+
+static void state(struct cpu *cpu){
+    printf("A: %d, B: %d, C: %d, D: %d\n", cpu->A, cpu->B, cpu->C, cpu->D);
+    printf("Status: %s\n", statusName(cpu->status));
+    printf("Result: %d\n", cpu->result);
+}
+
+
+int main(int argc, char *argv[]){
+    //compile as: gcc -o cpu main.c cpu.c
+    // run as: ./cpu progrma.bin
+    if(argc != 2){
+        printf("Invalid arguments");
+        return 1;
+    }
+
+    size_t stackCapacity = 256; //initail stack size
+    FILE *fptr;
+    fptr = fopen(argv[1], "rb");
+
+    int32_t *stackPtr;
+    int32_t *memory = cpuCreateMemory(fptr, stackCapacity, &stackPtr);
+    struct cpu cp;
+    cpuCreate(&cp, memory, stackPtr, stackCapacity);
+
+    //cpu run
+    cpuRun(&cp, INT_MAX);
+    state(&cp);
+
+    //memory management
+    fclose(fptr);
+    cpuDestroy(&cp);
+    return 0;
+
 }
