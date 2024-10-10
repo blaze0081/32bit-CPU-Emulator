cfc5391305166bcd5be47cc209ce2b424b91d846
blaze0081 <bhaskarmishra008@gmail.com>
2024-10-10 23:57:41 +0530

    initial files

---
+++ program/program.asm
@@ -0,0 +1,5 @@
+movr a 10    ; Move 10 to register A
+        movr b 5     ; Move 5 to register B
+        add b        ; Add B to A
+        out a        ; Output value in A
+        halt        ; Stop program
